10d0575c67f561b5f8c5845c0340796b7ef6b580
PatrikPatarcic <patrikizopatovca@gmail.com>
2024-04-13 13:56:35 +0200

    Initial commit

---
+++ Logic/Board.cs
@@ -0,0 +1,35 @@
+﻿namespace Logic
+{
+    public class Board
+    {
+        
+
+        
+    }
+
+    public class Location
+    {
+        int[] Row = Enumerable.Range(0, 8).ToArray();
+        int[] Coloumn = Enumerable.Range(0, 8).ToArray();
+        public Location(int row, int coloumn) 
+        {
+            Row[row] = row;
+            Coloumn[coloumn] = coloumn;
+        }
+
+    }
+
+    public abstract class Piece
+    {
+        bool hasMoved;
+        public int Row { get;}
+        public int Coloumn { get;}
+
+        enum Color
+        {
+            Black,
+            White
+        }
+
+    }
+}
